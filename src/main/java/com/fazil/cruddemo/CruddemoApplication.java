9f6a6d8592605050c03e94cc844d4afc99a3234b
Fazil1300 <mohamedfazil1300@gmail.com>
2024-03-13 15:29:32 +0530

    Sample commandlinerunner demo

--- src/main/java/com/fazil/cruddemo/CruddemoApplication.java
+++ src/main/java/com/fazil/cruddemo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.fazil.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -9,5 +11,10 @@ public class CruddemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
-
+@Bean
+public CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{
+			System.out.println("Hello, World!");
+		};
+}
 }
